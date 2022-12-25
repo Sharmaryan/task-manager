88d5ad25131402a42b66038a88cf3633f3ca8555
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-25 14:01:09 +0530

    connected to db

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
+const connectDB = require('./db/connect');
+require('dotenv').config();
 // middleware
 
 app.use(express.json());
 
 // routes
 
-app.get('/hello',(req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Task Manager App');
 })
 
-app.use('/api/v1/tasks',tasks);
+app.use('/api/v1/tasks', tasks);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server is listening on port ${port}`));
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+start();
 
 const port = 3000;
-app.listen(port, console.log(`server is listening on port ${port}`))
+
+
